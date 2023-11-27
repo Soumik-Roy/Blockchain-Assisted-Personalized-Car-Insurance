4016587f2e332b4b99498647809b82006c742f1e
samkitshah1262 <samkitshah1262@gmail.com>
2023-11-28 01:56:09 +0530

    truffle setup

---
+++ smartcontract/contracts/Algorithms.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.15;
+
+contract Algorithms{
+    uint256 N;
+    uint256 M;
+    uint256 S;
+    uint256 X;
+    uint256 DP;
+
+    uint256[] public NUM;
+
+    constructor(uint256 _N, uint256 _M, uint256 _S, uint256 _X, uint256 _DP) {
+        N = _N;
+        M = _M;
+        S = _S;
+        X = _X;
+        DP = _DP;
+
+        NUM = new uint256[](M);
+    }
+
+    function runAlgorithm() external {
+        uint256 j = 0;
+
+        for (uint256 i = 0; i < N; i++) {
+            uint256 P1 = calculateP(N, M, S, X, DP);
+            
+            if (1 == random(P1)) {
+                NUM[j] = i;
+                j = j + 1;
+                M = M - 1;
+            } else if (0 == random(P1)) {
+                M = M;
+            }
+
+            N = N - 1;
+
+            if (S > N - M) {
+                S = N - M;
+            } else if (S <= N - M) {
+                S = S;
+            }
+
+            if (M == j) {
+                break;
+            }
+        }
+    }
+
+    function calculateP(uint256 _N, uint256 _M, uint256 _S, uint256 _X, uint256 _DP) internal pure returns (uint256) {
+        // Implement the logic for calculating ˆP1 based on the parameters
+        // This function is a placeholder and should be replaced with the actual calculation
+        return (_N + _M + _S + _X + _DP) % 2;
+    }
+
+    function random(uint256 _input) internal view returns (uint256) {
+        // Implement your random number generation logic here
+        // This function is a placeholder and should be replaced with a secure random number generator
+        return uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, _input))) % 2;
+    }
+}
---
+++ smartcontract/migrations/2_deploy_contract.js
@@ -0,0 +1,6 @@
+
+var CarInsurance = artifacts.require("CarInsurance");
+ 
+module.exports = function(deployer) {
+    deployer.deploy(CarInsurance);
+};
